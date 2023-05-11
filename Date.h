ac2f20c5ca2f986689697d68c31cfcc3ec553533
Maxim <max@huzm.ru>
2023-05-11 21:28:38 +0300

    Date class improved

--- Date.h
+++ Date.h
@@ -1,3 +1,4 @@
+// Date.h
 #pragma once
 #include <iostream>
 #include <string>
@@ -11,25 +12,37 @@ private:
     UShort month;
     UShort year;
     std::string timezone;
+    static std::string month_names[13];
+    static short month_days[13];
 public:
     // constructors
     Date();
     Date(UShort d, UShort m, UShort y);
     // other methods
-    void print_short();       // prints date in the format: "DD.MM.YYYY";
-    void print_long();        // prints date in the format: "DD of MonthName, YYYY";
+    void print_short();       // prints date in the format: "D.MM.YYYY";
+    void print_long();        // prints date in the format: "D of MonthName, YYYY";
     void add_days(UInt d);    // adds <d> days to stored date
+};
+
+std::string Date::month_names[13]{
+    "", "Jan", "Feb", "March",
+        "Apr", "May", "June", "July",
+        "Aug", "Sept", "Okt", "Nov", "Dec"
+};
+
+short Date::month_days[13]{
+    0, 31,28,31,30,31,30,31,31,30,31,30,31
 };
 
 /*
  * Constructors
  */
 
-// Default constructor
+ // Default constructor
 Date::Date()
 {
     day = 1;
-    month = 2;
+    month = 1;
     year = 1970;
     timezone = "GMT+3 (Moscow)";
 }
@@ -44,4 +57,28 @@ Date::Date(UShort d, UShort m, UShort y) : Date()
     day = d;
     month = m;
     year = y;
+}
+
+// Print date in short format (D.MM.YYYY)
+void Date::print_short()
+{
+    std::cout << this->day << '.';
+    if (this->month < 10) std::cout << '0';
+    std::cout << this->month << '.';
+    std::cout << this->year << std::endl;
+}
+
+// Print date in long format (D of monthname YYYY)
+void Date::print_long()
+{
+    std::cout << this->day << " of ";
+    std::cout << month_names[month];
+    std::cout << ", " << this->year << std::endl;
+}
+
+// Adds <d> days to date (d<=365)
+void Date::add_days(UInt d)
+{
+    if (d > 365) return;
+    // Доп. домашнее задание
 }
